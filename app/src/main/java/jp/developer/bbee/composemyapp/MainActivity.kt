ca949ba157dbe4d7372d23245264cf771ce35d7b
BBee <atsushi.kokubun.47@gmail.com>
2022-03-26 16:09:59 +0900

    ComposeMyApp: Initial commit

---
+++ app/src/main/java/jp/developer/bbee/composemyapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package jp.developer.bbee.composemyapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import jp.developer.bbee.composemyapp.ui.theme.ComposeMyAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeMyAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeMyAppTheme {
+        Greeting("Android")
+    }
+}
